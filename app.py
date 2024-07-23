441a72c2ef2803f3fd7f005a7ae75c12448b7f96
saumil8200 <saumil8200@gmail.com>
2024-07-23 14:28:02 +0530

    UI changes

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def generate_qr():
     url = request.form['url']
     color = request.form['color']
 
-    # Generate QR code
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -23,14 +22,11 @@ def generate_qr():
     qr.add_data(url)
     qr.make(fit=True)
 
-    # Create an image from the QR Code instance with transparent background
     img = qr.make_image(fill_color=color, back_color="white")
 
-    # Save the image temporarily
     img_path = "temp_qr.png"
     img.save(img_path)
 
-    # Send the file as a downloadable attachment
     return send_file(img_path, as_attachment=True, download_name='qr_code.png')
 
 if __name__ == '__main__':
