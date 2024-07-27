41ddc46d35d04614456cd39c76d3281c716f3f4e
saumil8200 <saumil8200@gmail.com>
2024-07-27 12:17:14 +0530

    UI change

--- app.py
+++ app.py
@@ -9,25 +9,32 @@ def index():
     return render_template('index.html')
 
 @app.route('/generate_qr', methods=['POST'])
+@app.route('/generate_qr', methods=['POST'])
 def generate_qr():
-    url = request.form['url']
-    color = request.form['color']
+    try:
+        url = request.form['url']
+        color = request.form['color']
 
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
 
-    img = qr.make_image(fill_color=color, back_color="white")
+        img = qr.make_image(fill_color=color, back_color="white")
 
-    img_path = "temp_qr.png"
-    img.save(img_path)
+        img_path = "temp_qr.png"
+        img.save(img_path)
 
-    return send_file(img_path, as_attachment=True, download_name='qr_code.png')
+        return send_file(img_path, as_attachment=True, download_name='qr_code.png')
+    except KeyError as e:
+        return f"Missing data: {e}", 400
+    except Exception as e:
+        return f"An error occurred: {e}", 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
